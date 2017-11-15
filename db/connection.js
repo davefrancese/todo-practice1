ef0bd8c5cbab6eecbb40a3ff4726f45fc392e081
dfrancese <davefrancese@gmail.com>
2017-11-15 11:35:04 -0700

    omit

--- db/connection.js
+++
@@ -1,5 +0,0 @@
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile')[environment]
-require('dotenv').config()
-
-module.exports = require('knex')(config)
