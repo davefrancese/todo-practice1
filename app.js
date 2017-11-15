9b23825bf8c367f663995687fa33c2c117efaf6a
dfrancese <davefrancese@gmail.com>
2017-11-15 09:03:20 -0700

    heroku setup

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const todo = require('./routes/todo')
 const queries = require('./db/queries')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
 
@@ -9,7 +10,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true'
+    connection: process.env.DATABASE_URL
   }
 
 };
